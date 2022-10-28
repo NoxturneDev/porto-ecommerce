fca664dafec5e2eb948ad56101928f1ee6ad9191
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-10-28 21:46:53 +0700

    added necessary library

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+function LandingPage() {
+  return (
+    <>
+      <h1>HALO</h1>
+      <Link href='/product-detail'>Take a look at product detail</Link>
+    </>
+  )
+}
+
+export default LandingPage
