22c83220ab0a21d4ec4bb4266b2df516f61260e6
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-10-31 17:42:52 +0700

    created button ui component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,13 @@
 import Link from "next/link"
-import Nav from "../components/Nav"
+import { Button, BigButton } from "../components/Button"
 
 function LandingPage() {
   return (
     <>
-
+      <Button>add to cart</Button>
+      <Button type="outline">add to cart</Button>
+      <BigButton className="mx-7">View more</BigButton>
+      <BigButton type="outline">View more</BigButton>
     </>
   )
 }
