377e921b3913f469339e9d48cf884940c99135bc
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-11-01 01:29:19 +0700

    initialize homepage

--- components/Card.jsx
+++ components/Card.jsx
@@ -11,13 +11,13 @@ function Card({ data, ...props }) {
                     whileHover={{ scale: 1.2 }}
                     transition={{ type: "tween" }}
                     className="w-full h-full">
-                    <Image src="/img-01.png" alt="image" width={200} height={200} loading="lazy" />
+                    <Image src="/img-01.png" alt={data.name} width={200} height={200} loading="lazy" />
                 </motion.div>
             </div>
             {/* product description */}
             <div className="px-2 py-1 flex-col gap-2 my-2">
-                <h2 className='font-dm-sans font-semibold tracking-wider uppercase'>Product title</h2>
-                <h2 className='text-brand font-semibold'>$200.00</h2>
+                <h2 className='font-dm-sans font-semibold tracking-wider uppercase'>{data.name}</h2>
+                <h2 className='text-brand font-semibold'>${data.price}</h2>
             </div>
         </div>
     )
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,26 +2,67 @@ import Link from "next/link"
 import { Button, BigButton } from "../components/Button"
 import Card from "../components/Card"
 import { Input, Dropdown } from "../components/Input"
+
+
+
 function LandingPage() {
-  const arr = [1, 2, 3, 4, 5, 6]
+  const dummyData = [
+    {
+      productsId: 1,
+      name: 'Necklace 1',
+      detail: 'some good necklace for your wife',
+      price: '200',
+    },
+    {
+      productsId: 2,
+      name: 'Necklace 2',
+      detail: 'some good necklace for your wife',
+      price: '200',
+    },
+    {
+      productsId: 3,
+      name: 'Necklace 3',
+      detail: 'some good necklace for your wife',
+      price: '200',
+    },
+    {
+      productsId: 4,
+      name: 'Necklace 4',
+      detail: 'some good necklace for your wife',
+      price: '200',
+    },
+    {
+      productsId: 5,
+      name: 'Necklace 5',
+      detail: 'some good necklace for your wife',
+      price: '200',
+    },
+    {
+      productsId: 6,
+      name: 'Necklace 6',
+      detail: 'some good necklace for your wife',
+      price: '200',
+    }
+  ]
   return (
     <>
-      <Button>add to cart</Button>
-      <Button type="outline">add to cart</Button>
-      <BigButton className="mx-7">View more</BigButton>
-      <BigButton type="outline">View more</BigButton>
-      <br />
-      <div className="flex-col">
-        <Input className="mx-4"></Input>
-        <Input className="mx-4"></Input>
-        <Dropdown></Dropdown>
-      </div>
-      <div className="grid-cols-3 grid-rows-2">
-        {arr.map((card, i) => {
-          return (
-            <Card key={i}></Card>
-          )
-        })}
+      {/* jumbotron */}
+      <section className="w-full h-[70vh] px-10 py-2">
+        {/* slider here  */}
+        <div className="w-full h-full bg-basic-300"></div>
+      </section>
+      {/* gallery */}
+      <div className="flex-col justify-center items-center w-1/2 gap-4 h-max m-auto">
+        <div className="flex gap-4 my-4">
+          <Card data={dummyData[0]} />
+          <Card data={dummyData[1]} />
+          <Card data={dummyData[2]} />
+        </div>
+        <div className="flex gap-4 my-4">
+          <Card data={dummyData[3]} />
+          <Card data={dummyData[4]} />
+          <Card data={dummyData[5]} />
+        </div>
       </div>
     </>
   )
