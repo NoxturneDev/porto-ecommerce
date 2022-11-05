f83f2b1b3f0f79528cdb8f1ffc19ba6fd84ef8c2
ridhorayputraa <ridhorayputra@gmail.com>
2022-11-05 12:51:14 +0700

    finisihing contact page

---
+++ pages/account/index.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+
+const Account = () => {
+    return(
+        <Fragment>
+        <div className="flex ">
+            <h1>Testttt</h1>
+
+        </div>
+        </Fragment>
+    )
+}
+
+
+export default Account;
