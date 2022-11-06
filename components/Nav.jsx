0d096272e2d992db02418078379a1a51d5d05f86
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-11-07 01:24:19 +0700

    added necessary link

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,11 @@ const Nav = ({ children }) => {
         <main className="px-12">
             <nav className="flex justify-center items-center w-full min-w-max px-8 pt-4 pb-2 border-b-basic-300 border-b-2 mx-auto font-dm-sans mb-10">
                 <div className=" p-2 grow font-medium ">
-                    <h1 className='font-dm-sans text-brand text-xl tracking-wider'>TokoPaedi</h1>
+                    <h1 className='font-dm-sans text-brand text-xl tracking-wider'>
+                        <Link href="/">
+                            TokoPaedi
+                        </Link>
+                    </h1>
                 </div>
                 <div className="flex font-medium gap-12 px-6 border-r-basic-300 border-r-2 mr-4">
                     <Link href="/shop">Shop</Link>
@@ -16,7 +20,9 @@ const Nav = ({ children }) => {
                 <div className="flex justify-center items-center gap-8">
                     <Search size={24} />
                     <Cart size={24} />
-                    <Account size={24} />
+                    <Link href='/account'>
+                        <Account size={24} />
+                    </Link>
                 </div>
             </nav>
             {/* children component here */}
