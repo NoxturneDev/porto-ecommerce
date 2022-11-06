706ca3155b6081240b87d25fc70a489fde2ea8a1
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-11-06 20:56:43 +0700

    added onclick props to button styled component

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,28 +1,40 @@
-export const Button = ({ children: text = "TEXT", type, className }) => {
+export const Button = ({ children: text = "TEXT", type, className, onClick, ...props }) => {
     if (type === "outline")
         return (
-            <button className={`min-w-[20rem] px-20 py-1 rounded-sm border-2 border-black font-semibold hover:bg-black hover:text-white transition-all duration-300 uppercase ${className ? className : ''} `}>
+            <button 
+            onClick={e => onClick(e)}
+            {...props}
+            className={`min-w-[20rem] px-20 py-1 rounded-sm border-2 border-black font-semibold hover:bg-black hover:text-white transition-all duration-300 uppercase ${className ? className : ''} `}>
                 {text}
             </button>
         )
     else
         return (
-            <button className={`min-w-[20rem] px-20 py-1 rounded-sm border-2 border-black font-semibold bg-black text-white hover:bg-white hover:text-black transition-all duration-300 uppercase ${className ? className : ''} `}>
+            <button 
+            onClick={e => onClick(e)}
+            {...props}
+            className={`min-w-[20rem] px-20 py-1 rounded-sm border-2 border-black font-semibold bg-black text-white hover:bg-white hover:text-black transition-all duration-300 uppercase ${className ? className : ''} `}>
                 {text}
             </button>
         )
 }
 
-export const BigButton = ({ children: text = "TEXT", type, className }) => {
+export const BigButton = ({ children: text = "TEXT", type, className, onClick, ...props }) => {
     if (type === "outline")
         return (
-            <button className={`min-w-[10rem] px-8 py-3 rounded-sm  border-2   border-black font-semibold hover:bg-black hover:text-white transition-all duration-300  ${className ? className : ''} `}>
+            <button 
+            onClick={e => onClick(e)}
+            {...props}
+            className={`min-w-[10rem] px-8 py-3 rounded-sm  border-2   border-black font-semibold hover:bg-black hover:text-white transition-all duration-300  ${className ? className : ''} `}>
                 {text}
             </button>
         )
     else
         return (
-            <button className={`min-w-[10rem] px-8 py-3 rounded-sm border-2 border-black font-semibold bg-black text-white hover:bg-white hover:text-black transition-all duration-300  ${className ? className : ''} `}>
+            <button 
+            onClick={e => onClick(e)}
+            {...props}
+            className={`min-w-[10rem] px-8 py-3 rounded-sm border-2 border-black font-semibold bg-black text-white hover:bg-white hover:text-black transition-all duration-300  ${className ? className : ''} `}>
                 {text}
             </button>
         )
