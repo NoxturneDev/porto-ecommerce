bb8fc7ed24051791da988065fd55629c9f010ffe
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-11-13 02:16:03 +0700

    added config object for api fetch

--- utils/commerce.js
+++ utils/commerce.js
@@ -1,5 +1,28 @@
 import Commerce from "@chec/commerce.js";
 
-const commerce = new Commerce(process.env.NEXT_PUBLIC_COMMERCEJS_API_KEY, true)
+// commerce js SDK 
+export const commerce = new Commerce(process.env.NEXT_PUBLIC_COMMERCEJS_API_KEY, true)
 
-export default commerce
+// api fetch  to commerce js configurtion
+const url = "https://api.chec.io/v1/products"
+
+export const config = {
+    public: {
+        headers: {
+            "X-Authorization": process.env.NEXT_PUBLIC_COMMERCEJS_API_KEY,
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        url: url
+    },
+    secret: {
+        headers: {
+            "X-Authorization": process.env.NEXT_PUBLIC_COMMERCEJS_SECRET_API_KEY,
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        url: url
+    }
+
+}
+
