956af44ecf2badcd12e0e2fc7c876d9126540e4f
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-11-12 22:38:13 +0700

    new api routes

--- pages/api/login.js
+++
@@ -1,3 +0,0 @@
-export default function handler(req, res) {
-    res.send({text : 'anjay mabar gan'})
-}
--- pages/api/product/index.js
+++ pages/api/product/index.js
@@ -1,6 +1,4 @@
-import Commerce from "@chec/commerce.js"
-
-const commerce = new Commerce('pk_test_4831638fed30513ccbe1f0454b201762b05e88bf54a43', true)
+import commerce from "@/utils/commerce"
 
 export default async function productHandler(req, res) {
     const { method } = req
